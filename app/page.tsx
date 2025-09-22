e5f3d6d1e286dd39eb13010f3636aaca4413ada5
Wen Zhuo <wenzhuo0228@gmail.com>
2025-09-22 14:17:21 +0800

    aasa

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            i can see  <code>app/page.tsx</code>.
+            i can see you <code>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
