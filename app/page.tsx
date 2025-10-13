305076f2529ebb8a5118ca29963687ca53854106
Wen Zhuo <wenzhuo0228@gmail.com>
2025-10-13 11:25:51 +0800

    card

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,53 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import {useState} from 'react';
+import Button from '@mui/material/Button';
+import {Card, CardActions, CardHeader, CardContent} from '@mui/material';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count+1);
+    alert(count);
+  }
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            i can two  you <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  <Container >
+    <Card>
+      <CardHeader title="張文卓" />
+      <CardContent>
+        <Typography variant="body1">資管四甲 411401011</Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="contained" onClick={handleClick}>Click Me</Button>
+      </CardActions>
+    </Card>
+    <Card>
+      <CardHeader title="雷家齊" />
+      <CardContent>
+        <Typography variant="body1">資管四甲 411401463</Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="contained" onClick={handleClick}>Click Me</Button>
+      </CardActions>
+    </Card>
+    <Card>
+      <CardHeader title="林岳霆" />
+      <CardContent>
+        <Typography variant="body1">資管四甲 411401449</Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="contained" onClick={handleClick}>Click Me</Button>
+      </CardActions>
+    </Card>
+    <Card>
+      <CardHeader title="林明君" />
+      <CardContent>
+        <Typography variant="body1">資管四甲 411401578</Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="contained" onClick={handleClick}>Click Me</Button>
+      </CardActions>
+    </Card>
+  </Container>)
 }
